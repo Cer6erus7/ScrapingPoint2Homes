e85fd8791f8c1a10859493cbd42712d18b6d34a9
Roker <matvey14481@gmail.com>
2023-08-15 23:16:58 -0700

    Made worked dictionary, rest is a logic

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import pprint
 import requests
 from bs4 import BeautifulSoup
 
 
 URL = "https://www.point2homes.com/CA/Apartments-For-Rent/BC.html"
-HOST = "https://www.point2homes.com/"
+HOST = "https://www.point2homes.com"
 HEADERS = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"}
 
@@ -19,9 +20,34 @@ def get_content(response):
     for card in cards:
         title = card.find('div', class_="address-container").text.strip()
 
+        try:
+            area = card.find('li', class_="ic-sqft").text.strip()[:-4]
+        except AttributeError:
+            area = None
+
+        try:
+            bath = card.find('li', class_="ic-baths").text.strip()[:-2]
+        except AttributeError:
+            bath = None
+
+        try:
+            beds = card.find('li', class_="ic-beds").text.strip()
+        except AttributeError:
+            beds = None
+        else:
+            beds = beds.replace('Bds', '')
+            beds = beds.replace('Bd', '')
+
         dct = {
             "title": title[:title.index("\r")].replace('  ', ''),
-            'address': None
+            'address': card.find('div', class_="address-container").get("data-address"),
+            'price': card.find("div", class_="price has-rental-term").get('data-price'),
+            'link': HOST + card.find("div", class_="inner-right").find("a").get("href"),
+            'img_link': card.find("div", class_="lslide active").find('img').get('src'),
+            'apartments_type': card.find("li", class_="property-type ic-proptype").text.strip(),
+            "area": area,
+            "bath": bath,
+            "beds": beds
         }
         yield dct
 
@@ -29,4 +55,4 @@ def get_content(response):
 if __name__ == '__main__':
     response = get_request(URL)
     for i in get_content(response):
-        print(i)
+        pprint.pprint(i)
